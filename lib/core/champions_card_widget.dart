bbaf602af6bef8fe2ab5955d75951c9ef048f8f1
Oguzhan Yildirim <oguzhany1337@gmail.com>
2022-09-29 20:17:22 +0300

    Champions Card Widget Updated.

--- lib/core/champions_card_widget.dart
+++ lib/core/champions_card_widget.dart
@@ -1,27 +1,30 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import '../constants/text_style.dart';
 
 class ChampionsCardWidget extends StatelessWidget {
-	const ChampionsCardWidget({super.key, required this.championName, required this.championRole, required this.championCost});
-	final String championName;
-	final String championRole;
-	final String championCost;
-	@override
-	Widget build(BuildContext context) {
-		return Card(
-              child: ListTile(
-                leading: GetChampions(
-                  championName: championName,
-                ),
-                title: Text(
-                  championName,
-                  style: textStyle(),
-                ),
-							subtitle: Text(championRole),
-							trailing: Text(championCost),
-              ),
-            );
-	}
+  const ChampionsCardWidget(
+      {super.key, required this.championName, required this.championRole, required this.championCost});
+  final String championName;
+  final String championRole;
+  final String championCost;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: GetChampions(
+          championName: championName,
+        ),
+        title: Text(
+          championName,
+          style: textStyle(),
+        ),
+        subtitle: Text(championRole),
+        trailing: Text(championCost),
+      ),
+    );
+  }
 }
 
 class GetChampions extends StatelessWidget {
