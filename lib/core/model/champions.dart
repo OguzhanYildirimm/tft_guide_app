d76c324ef2edc88b5034d9028f8cae8db8029102
Oguzhan Yildirim <oguzhany1337@gmail.com>
2022-09-30 18:15:57 +0300

    champions.dart updated

--- lib/core/model/champions.dart
+++ lib/core/model/champions.dart
@@ -9,17 +9,20 @@ class Champion {
   int cost;
 	List<String>? traits;
 
-  Champion({required this.name, required this.championId, required this.cost});
+  Champion({required this.name, required this.championId, required this.cost,required this.traits});
 
   factory Champion.fromMap(Map<String, dynamic> json) => Champion(
         name: json["name"],
 				championId: json["championId"],
 				cost: json["cost"],
+				traits: json['traits'].cast<String>(),
+				
       );
 
   Map<String, dynamic> toMap() => {
         "name": name,
         "championId": championId,
         "cost": cost,
+				"traits" : traits
       };
 }
