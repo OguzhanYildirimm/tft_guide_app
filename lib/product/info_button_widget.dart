2c01ed7ba0230a3396882acc465c70d021b7ad0d
Oguzhan Yildirim <oguzhany1337@gmail.com>
2022-10-03 23:59:34 +0300

    info_button added.

--- lib/product/info_button_widget.dart
+++ lib/product/info_button_widget.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class InfoButton extends StatelessWidget {
+	const InfoButton({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return Container();
+	}
+}
